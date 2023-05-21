1c13b72d1d78d8b7127da9143dbc552f38476e32
taran-duba <tkduba19@gmail.com>
2023-05-21 17:23:46 -0400

    changed title

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'ChatGPT',
+  title: 'Google Classroom',
   description: 'An unofficial GPT 3.5 Model',
 }
 
